1b6bf63bfff96ebd8dc2fb00bd1ddfae54b1ee97
Joao Vitor Maciel Lima <vitir_maciel2020@outlook.com>
2024-12-20 00:02:46 -0300

    melhoria do meu código, mas apresentando warning(AVISO)

--- comprimento.c
+++ comprimento.c
@@ -1,21 +1,40 @@
-#include <stdio.h> // Adicionando biblioteca para ler inputs e outputs
-#include <stdlib.h>
+#include <stdio.h> //  biblioteca para ler inputs e outputs
+#include <stdlib.h> // biblioteca para usar alguns comando especificos como por exemplo na implementação do código os System("cls").
+#include <string.h> // biblioteca utilizada para manipulação de strings.
 
-int main(){ // função main resposável por executar o programa.
-    int opcao; 
+void menu(){ // função void que retorna vazio
+    int ler_menu;
+    do{
 
-    do{ // estrutura repetição para melhoria do MENU
-    system("cls");
+        printf("---------MENU Conversor---------\n");
+        printf("1 - Converter centrimetros para metros\n");
+        printf("2 - Converter metros para centimetros\n");
+        printf("3 - Converter milimetros para metros\n");
+        printf("4 - Sair\n");
     
-    printf("---------MENU Conversor---------\n");
-    printf("1 - Converter centrimetros para metros\n");
-    printf("2 - Converter metros para centimetros\n");
-    printf("3 - Converter milimetros para metros\n");
-    printf("4 - Sair\n");
-    printf("Informe uma opcao:\n");
+        printf("Informe uma opcao:\n");
+        scanf("%d", &ler_menu);
 
-    scanf("%d", &opcao);
 
-    }while(opcao >= 1 && opcao <= 3);
-    
+        switch(ler_menu){
+            char opcao;
+            case 1:
+            // operacao
+
+            printf("Deseja retornar ao menu? [Sim] ou [Nao]\n");
+            scanf("%c",&opcao);
+
+            if(opcao = "Sim"){
+                printf("teste");
+            }
+            break;
+        }
+    }while(ler_menu >=1 && ler_menu <= 4);
+
 }
+
+int main(){ // função main resposável por executar o programa.    
+
+    menu();
+}
+
