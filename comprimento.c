bd3d9ed1e0024408c82ec6c0c06ee1bc23a6df48
Joao Vitor Maciel Lima <vitir_maciel2020@outlook.com>
2024-12-19 21:43:47 -0300

    Adicionando o arquivo comprimento.c

---
+++ comprimento.c
@@ -0,0 +1,15 @@
+#include <stdio.h> // Adicionando biblioteca para ler inputs e outputs
+
+
+int main(){ // função main resposável por executar o programa.
+    int opcao; 
+
+
+    printf("---------MENU Conversor---------\n");
+    printf("1 - Converter centrimetros para metros\n");
+    printf("2 - Converter metros para centimetros\n");
+    printf("3 - Converter milimetros para metros\n");
+    printf("Informe uma opcao:\n");
+    scanf("%d", &opcao);
+    
+}
