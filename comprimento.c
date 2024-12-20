8d0d48e231251195155648ab512a0202594d0b45
Joao Vitor Maciel Lima <vitir_maciel2020@outlook.com>
2024-12-19 22:42:01 -0300

    Implementação de estruturas de repitção para melhoria do desenvolvimento do programa

--- comprimento.c
+++ comprimento.c
@@ -1,15 +1,21 @@
 #include <stdio.h> // Adicionando biblioteca para ler inputs e outputs
-
+#include <stdlib.h>
 
 int main(){ // função main resposável por executar o programa.
     int opcao; 
 
-
+    do{ // estrutura repetição para melhoria do MENU
+    system("cls");
+    
     printf("---------MENU Conversor---------\n");
     printf("1 - Converter centrimetros para metros\n");
     printf("2 - Converter metros para centimetros\n");
     printf("3 - Converter milimetros para metros\n");
+    printf("4 - Sair\n");
     printf("Informe uma opcao:\n");
+
     scanf("%d", &opcao);
+
+    }while(opcao >= 1 && opcao <= 3);
     
 }
